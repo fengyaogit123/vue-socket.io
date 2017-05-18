6c04ed3c63d1832e346a560044bda7e36e18a630
beautifulBoys <1298947916@qq.com>
2017-05-18 10:07:55 +0800

    优化代码结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
 import VueRounter from 'vue-router';
+import App from './App';
 import Login from './components/login/login';
 import Room from './components/room/room';
 
