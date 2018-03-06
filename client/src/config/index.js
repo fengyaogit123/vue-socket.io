8b3b1d06f9631d9ebaa415173c283ef894cab2f5
beautifulBoys <1298947916@qq.com>
2018-03-06 22:20:22 +0800

    初步部署测试

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,4 +1,4 @@
-export const socketUrl = 'http://192.168.31.42:3003';
+export const socketUrl = 'http://47.95.212.47:3003';
 export const tourismUrl = 'http://www.baidu.com';
 export const robotId = 10;
 export const groupId = 20;
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose';
 
 function dbconnect () {
-  mongoose.connect('mongodb://127.0.0.1:27017/tourism', {useMongoClient: true});
+  mongoose.connect('mongodb://lixin:123123@127.0.0.1:27017/tourism', {useMongoClient: true});
   var db = mongoose.connection;
   db.on('error', () => {
     console.log('数据库连接出错。');
@@ -11,7 +11,7 @@ function dbconnect () {
     console.log('数据库连接成功。');
   });
   db.on('close', () => {
-    console.log('数据库断开，重新连接数据库');
+    console.log('数据库已断开，请重新连接数据库');
   });
 }
 
